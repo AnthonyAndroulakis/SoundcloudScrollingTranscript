947dfdd7779b5d94bf65952b1681c563cad59dee
Anthony Androulakis <aandroulakis@zoho.com>
2020-02-05 11:27:37 -0500

    updated to include soundcloud track num metadata

--- transcriptparser.py
+++ transcriptparser.py
@@ -4,7 +4,8 @@ import sys
 def run(filename): #example input: captions.en.csv
 	#first parse csv file for relevant data
 	lines = open(filename).read().split('\n')
-	data = [i.split(';') for i in lines]
+	data = [i.split(';') for i in lines][1:]
+	tracknum = lines[0].split(';')[0]
 	seconds = [[int(k) for k in j[0].split(':')][0]*3600+[int(k) for k in j[0].split(':')][1]*60+[int(k) for k in j[0].split(':')][2]+[int(k) for k in j[0].split(':')][3]*0.1 for j in data] #ignore first
 	words = [l[1] for l in data] #ignore first
 	timeDisplay = [m[0][:-2] for m in data]
@@ -17,7 +18,9 @@ def run(filename): #example input: captions.en.csv
 		raise TranscriptError("LENGTH needs to be greater than last time mark.")
 
 	#create txt file with html-version of transcript
-	htmltext = '<div class="wavesurfer-transcript">' #ending = </div>
+	htmltext = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+tracknum+'&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'
+	htmltext += '<h3 style="font-family:arial;"> Transcript: </h3>'
+	htmltext += '<div class="wavesurfer-transcript">' #ending = </div>
 	for i in range(len(data)-1): #look at i+1
 		if i==len(data)-2: #if at last element
 			htmltext += '<div class="wavesurfer-marker" data-start="'+str(seconds[i+1])+'" data-end="'+str(seconds[0])+'"> <table><tr><td>'+timeDisplay[i+1]+'&nbsp;&nbsp;</td> <td>'+words[i+1]+'</td></tr></table></div>'
